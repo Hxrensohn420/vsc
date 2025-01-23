0623c71dee9fcf650214f4c9b8e91cf48f7a2acf
Hxrensohn420 <95819980+Hxrensohn420@users.noreply.github.com>
2025-01-23 02:48:59 +0100

    Add files via upload

---
+++ findomain.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+curl -LO https://github.com/findomain/findomain/releases/latest/download/findomain-linux.zip
+unzip findomain-linux.zip
+chmod +x findomain
+sudo mv findomain /usr/bin/findomain
+findomain --help
